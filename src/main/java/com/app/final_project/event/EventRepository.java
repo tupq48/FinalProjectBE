595ce3895103e1a955b54c1b117d6e78367a0673
YukiVt <xuanlong1234321@gmail.com>
2024-04-23 21:45:57 +0700

    FIN-29: update performance of view event

--- src/main/java/com/app/final_project/event/EventRepository.java
+++ src/main/java/com/app/final_project/event/EventRepository.java
@@ -1,8 +1,15 @@
 package com.app.final_project.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
+
+    @Query(value = "CALL GetEventsByPage(:limit, :offset)", nativeQuery = true)
+    List<Object[]> findEventsByPage(@Param("limit") int limit, @Param("offset") int offset);
 }
