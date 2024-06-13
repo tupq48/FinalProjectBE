4f4d1608cfc56adc60744bbc293cbcce04b799ea
YukiVt <xuanlong1234321@gmail.com>
2024-06-13 08:00:22 +0700

    FIN-Notification: create notification for user

---
+++ src/main/java/com/app/final_project/notification/NotificationRepository.java
@@ -0,0 +1,12 @@
+package com.app.final_project.notification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+    List<Notification> findAllByUserId(Integer userId);
+
+}
