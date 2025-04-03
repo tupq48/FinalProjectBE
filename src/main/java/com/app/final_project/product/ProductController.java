c98a41c0abf30a2c839efcf30c6b95868e2be525
hieupc <chinhhieu0906@gmail.com>
2025-04-03 16:43:52 +0700

    update code RabbitMQ

--- src/main/java/com/app/final_project/product/ProductController.java
+++ src/main/java/com/app/final_project/product/ProductController.java
@@ -2,6 +2,7 @@ package com.app.final_project.product;
 
 import com.app.final_project.event.Event;
 import com.app.final_project.event.dto.EventRequest;
+import com.app.final_project.producer.MessageProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +14,16 @@ import java.util.List;
 @RestController
 @RequestMapping("api/product")
 public class ProductController {
+    private final ProductService productService;
+    private final MessageProducer producer;
+    private final TelegramLogService telegramLogService;
+
     @Autowired
-    private ProductService productService;
+    public ProductController(ProductService productService, MessageProducer producer, TelegramLogService telegramLogService) {
+        this.productService = productService;
+        this.producer = producer;
+        this.telegramLogService = telegramLogService;
+    }
 
     @GetMapping("/products")
     public List<Product> getAllProducts() {
@@ -30,5 +39,25 @@ public class ProductController {
     @GetMapping("/{id}")
     public Product getProductById(@PathVariable int id) {
         return productService.getProductById(id);
+    }
+
+    @GetMapping("/send/{msg}")
+    public String sendMessage(@PathVariable String msg) {
+        producer.sendMessage(msg);
+        return "Message sent: " + msg;
+    }
+    @GetMapping("/send1")
+    public void sendMessage1() {
+        producer.sendMessageFanout(); // Không cần routing key
+    }
+    @PostMapping("/sendEmail")
+    public ResponseEntity<String> sendNotification(@RequestParam String email) {
+        producer.sendEmailNotification(email, "Test Email", "This is a test notification.");
+        return ResponseEntity.ok("Email notification sent to RabbitMQ.");
+    }
+    @PostMapping("/sendTele")
+    public String sendMessageTele(@RequestParam String message) {
+        producer.sendMessageToQueue(message);
+        return "Message sent to Producer: " + message;
     }
 }
